c15626dd79b460b56884465163f4b593186d7085
TobiToxota <tobias.seiler@posteo.de>
2022-06-21 14:02:21 +0200

    prepared backend folders for nightout app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'nightout.apps.NightoutConfig',
     'base.apps.BaseConfig',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
